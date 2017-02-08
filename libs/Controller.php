d1b7ee5cb44cf072f4cc347f64867cbf90afd5ce
gmg-907 <medynskyypavlo@gmail.com>
2017-02-08 11:06:33 +0200

    added lost commit

---
+++ libs/Controller.php
@@ -0,0 +1,20 @@
+<?php
+class Controller
+{
+    public function __construct() {
+        $this->view = new View();
+    }
+
+    public function loadModel($name)
+    {
+        $path = 'models/' . $name . '_Model.php';
+        if (file_exists($path)) {
+            require 'models/' . $name . '_Model.php';
+            $modelName = $name . '_Model';
+            $this->model = new $modelName();
+        } else {
+            require 'models/File_MOdel.php';
+        }
+    }
+}
+?>
